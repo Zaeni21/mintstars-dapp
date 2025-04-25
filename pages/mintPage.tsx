0fdc09c7616aba06a1cfb676e8cacf88367836b3
Jawir <68275706+Zaeni21@users.noreply.github.com>
2025-04-26 02:42:03 +0700

    Delete pages/mintPage.tsx

--- pages/mintPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function MintPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold">Mint Page (Replace with your real NFT Generator)</h1>
-    </div>
-  );
-}
