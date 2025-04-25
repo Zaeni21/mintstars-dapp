99d017f57b3ce7b30c86aef239523e5064e37f89
Jawir <68275706+Zaeni21@users.noreply.github.com>
2025-04-26 04:11:43 +0700

    Add files via upload

---
+++ pages/mintPage.tsx
@@ -0,0 +1,7 @@
+export default function MintPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">Mint Page (Replace with your real NFT Generator)</h1>
+    </div>
+  );
+}
